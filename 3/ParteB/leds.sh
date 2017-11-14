dec22358fbe57f546c6cb1ff4a34a37b1872a3d0
ancral <angcruz@ucm.es>
2017-11-14 18:07:51 +0100

    Update leds.sh

--- 3/ParteB/leds.sh
+++ 3/ParteB/leds.sh
@@ -1,13 +1,32 @@
 op=$1
 
 ram() {
-	echo "pasa"
+	if [ ! lsmod | grep blinkdrv > /dev/null ] ; then
+    		echo "-----------------------------"
+		echo "Instalando el driver blinkdrv"
+		echo "-----------------------------"
+		if [ -f ../auto.sh | -f auto.sh ]; then
+			if [[ $EUID -ne 0 ]] ; then
+			   echo "Debes de ser root para instalar el driver" 1>&2
+			   echo "-----------------------------------------"
+			   exit 1
+			fi
+			if [ ../auto.sh > /dev/null ] ; then
+				./auto.sh
+			fi
+		else
+			echo "auto.sh no encontrado, instale manualmente el driver"
+			echo "----------------------------------------------------"
+			exit 1
+		fi	
+	fi
+	
+	gcc blink_user.c -o blink
 	while(true); do
-		sleep 2;
+		sleep 1;
 		res=$(free | grep Mem | awk '{print $3/$2 * 100.0}');
-		gcc blink_user.c -o blink
 		./blink $res
-		sleep 2;
+		sleep 1;
 	done
 
 }
@@ -17,8 +36,8 @@ case $op in
 	ram
 	;;
 	*)
-	echo "-----------------"
-	echo "Usa -ram..."
-	echo "-----------------"
+	echo "----------------------"
+	echo "Escribe ./leds.sh -ram"
+	echo "----------------------"
 	;;
 esac
