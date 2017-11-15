3fd70158a2c2ad7ef323f09dd45caa45e598e9b9
ancral <angcruz@ucm.es>
2017-11-15 15:57:38 +0100

    Update blink_user.c

--- 3/ParteB/blink_user.c
+++ 3/ParteB/blink_user.c
@@ -6,13 +6,8 @@
 
 int main( int argc, char** argv ) {
 	char* leds;
-	if(argc != 2){
-		printf("Error: arg != 2\n");
-		exit(1);
-    }
-
-    float porcentaje = atof(argv[1]);
-    //printf("%f", porcentaje);
+	char buff[512];
+	FILE *inp; 
     
 	FILE* f = fopen("/dev/usb/blinkstick0", "wb");
 
@@ -20,29 +15,38 @@ int main( int argc, char** argv ) {
 	    printf("Error: opening file!\n");
 	    exit(1);
 	}
-
-    leds = "0:000000,1:000000,2:000000,3:000000,4:000000,5:000000,6:000000,7:000000";
-    fwrite(leds,strlen(leds),1,f);
-
-    if(porcentaje > 87.5) {
-        leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100,6:111000,7:110000";
-    }else if(porcentaje > 75) {
-        leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100,6:111000";
-    }else if(porcentaje > 62.5) {
-        leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100";
-    }else if(porcentaje > 50) {
-        leds = "0:000011,1:000111,2:001111,3:001110,4:001100";
-    }else if(porcentaje > 37.5) {
-        leds = "0:000011,1:000111,2:001111,3:001110";
-    }else if(porcentaje > 25) {
-        leds = "0:000011,1:000111,2:001111";
-    }else if(porcentaje > 12.5) {
-        leds = "0:000011,1:000111";
-    }else if(porcentaje > 0) {
-        leds = "0:000011";
-    }
 	
-    fwrite(leds,strlen(leds),1,f);
+	while(1) {
+		if(!(inp = popen("free | grep Mem | awk '{print $3/$2 * 100.0}'", "r"))) exit(1);
+		
+		fgets(buff, sizeof(buff), inp);
+		
+		float porcentaje = atof(buff);
+		
+		leds = "0:000000,1:000000,2:000000,3:000000,4:000000,5:000000,6:000000,7:000000";
+		fwrite(leds,strlen(leds),1,f);
+		
+		if(porcentaje > 87.5) {
+			leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100,6:111000,7:110000";
+		}else if(porcentaje > 75) {
+			leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100,6:111000";
+		}else if(porcentaje > 62.5) {
+			leds = "0:000011,1:000111,2:001111,3:001110,4:001100,5:011100";
+		}else if(porcentaje > 50) {
+			leds = "0:000011,1:000111,2:001111,3:001110,4:001100";
+		}else if(porcentaje > 37.5) {
+			leds = "0:000011,1:000111,2:001111,3:001110";
+		}else if(porcentaje > 25) {
+			leds = "0:000011,1:000111,2:001111";
+		}else if(porcentaje > 12.5) {
+			leds = "0:000011,1:000111";
+		}else if(porcentaje > 0) {
+			leds = "0:000011";
+		}
+		
+		fwrite(leds,strlen(leds),1,f);
+		pclose(inp);
+	}
     fclose(f);      
     return 0;
 }
