27e830c41442767020f8c8cdd4ef88e09ca0e143
ancral <angcruz@ucm.es>
2017-11-29 15:43:11 +0100

    Update consumidor.sh

--- 4/consumidor.sh
+++ 4/consumidor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-while [[ true ]]; do
- cat /proc/modlist/;
- sleep 1;
+n=0
+
+while [ "$n" -ne 20 ]; do
+n=$((n+1));
+cat /proc/modlist;
+sleep 0.00001;
+echo remove "$n" > /proc/modlist;
 done
 
 
